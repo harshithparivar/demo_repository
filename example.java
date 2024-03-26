6c85c1fdd2c56d63e9dd62cbb71a79d8a71079d8
harshithparivar <harshithg7356@gmail.com>
2024-03-26 09:52:32 +0530

    initial stage

---
+++ example.java
@@ -0,0 +1,12 @@
+class example
+{
+public static void main(String args[])
+{
+String str1=new String("my name is Harshith ");
+char c;
+c=str1.charAt(11);
+System.out.println(c);
+}
+}
+
+          
---
+++ example3.java
@@ -0,0 +1,11 @@
+class example4
+{
+public static void main(String args[])
+{
+String str=new String("Puttur");
+String s=str replace('u','a');
+System.out.println(s);
+}
+}
+
+          
